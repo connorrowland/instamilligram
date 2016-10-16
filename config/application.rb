65b00772b0f2c3fcd362d99cdc95a055cf256756
Connor Rowland <cjr6734@yahoo.com>
2016-10-15 22:18:03 -0400

    Trying to fix precompiler issue

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,6 @@ Bundler.require(*Rails.groups)
 
 module InstaMilligram
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -24,5 +23,6 @@ module InstaMilligram
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
   end
 end
